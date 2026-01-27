1f4f6720f97f889f7af5d2f0ddf9b6a348f8b053
Anderson Brito de Carvalho <andersoncarv@hotmail.com>
2026-01-27 20:53:04 -0300

    Exercicio 04

--- exercicios_01.py
+++ exercicios_01.py
@@ -16,11 +16,18 @@
 
 # print(f" O Resto da divisão do número {numero_informado}, por 5 é igual a {resto_divisao}")
 
-## 3 - Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+# ## 3 - Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+# numero_1 = int(input("Digite o primeiro valor:"))
+# numero_2 = int(input("Digite o segundo valor:"))
+
+# valor_multiplicacao = numero_1 * numero_2
+
+# print(f"A Multiplicação entre o valor {numero_1} e o valor {numero_2} é igual a {valor_multiplicacao}")
+
+## 4 - Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 
 numero_1 = int(input("Digite o primeiro valor:"))
 numero_2 = int(input("Digite o segundo valor:"))
-
-valor_multiplicacao = numero_1 * numero_2
-
-print(f"A Multiplicação entre o valor {numero_1} e o valor {numero_2} é igual a {valor_multiplicacao}")
+valor_divisao_inteira = numero_1 // numero_2
+print(f"A Divisão inteira entre o valor {numero_1} e o valor {numero_2} é igual a {valor_divisao_inteira}")
