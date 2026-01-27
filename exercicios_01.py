c5fd70a4a541ede17b1ea779dbb6d3e5a664aabf
Anderson Brito de Carvalho <andersoncarv@hotmail.com>
2026-01-27 20:46:29 -0300

    Exercio 02

--- exercicios_01.py
+++ exercicios_01.py
@@ -1,8 +1,17 @@
-# 1 - Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+# # 1 - Escreva um programa que soma dois números inteiros inseridos pelo usuário.
 
-numero_1 = int(input("Digite o primeiro valor: "))
-numero_2 = int(input("Digite o segundo valor: "))
+# numero_1 = int(input("Digite o primeiro valor: "))
+# numero_2 = int(input("Digite o segundo valor: "))
 
-valor_total = numero_1 + numero_2
+# valor_total = numero_1 + numero_2
 
-print(f" A Soma entre o valor {numero_1} e o valor {numero_2} é igual a {valor_total}")
+# print(f" A Soma entre o valor {numero_1} e o valor {numero_2} é igual a {valor_total}")
+
+
+# # 2 - Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+numero_informado = int(input("Digite um número:"))
+
+resto_divisao = numero_informado % 5
+
+print(f" O Resto da divisão do número {numero_informado}, por 5 é igual a {resto_divisao}")
