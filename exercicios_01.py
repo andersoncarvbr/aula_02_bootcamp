c7b74e1c8a67a648c1bb1f2d25df63a7d50f4ffc
Anderson Brito de Carvalho <andersoncarv@hotmail.com>
2026-01-27 20:55:49 -0300

    Exercicio 05

--- exercicios_01.py
+++ exercicios_01.py
@@ -25,9 +25,17 @@
 
 # print(f"A Multiplicação entre o valor {numero_1} e o valor {numero_2} é igual a {valor_multiplicacao}")
 
-## 4 - Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+# ## 4 - Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 
-numero_1 = int(input("Digite o primeiro valor:"))
-numero_2 = int(input("Digite o segundo valor:"))
-valor_divisao_inteira = numero_1 // numero_2
-print(f"A Divisão inteira entre o valor {numero_1} e o valor {numero_2} é igual a {valor_divisao_inteira}")
+# numero_1 = int(input("Digite o primeiro valor:"))
+# numero_2 = int(input("Digite o segundo valor:"))
+# valor_divisao_inteira = numero_1 // numero_2
+# print(f"A Divisão inteira entre o valor {numero_1} e o valor {numero_2} é igual a {valor_divisao_inteira}")
+
+
+## 5 - Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+
+numero_informado = int(input("Digite um número:"))
+valor_quadrado = numero_informado ** 2
+print(f"O Quadrado do número {numero_informado} é igual a {valor_quadrado}")
+
