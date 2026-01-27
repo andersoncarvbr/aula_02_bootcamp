9331e0f6a337c9650811e61ad585fdc5ae3b5d5a
Anderson Brito de Carvalho <andersoncarv@hotmail.com>
2026-01-27 20:41:36 -0300

    Exercicio 01

---
+++ exercicios_01.py
@@ -0,0 +1,8 @@
+# 1 - Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+numero_1 = int(input("Digite o primeiro valor: "))
+numero_2 = int(input("Digite o segundo valor: "))
+
+valor_total = numero_1 + numero_2
+
+print(f" A Soma entre o valor {numero_1} e o valor {numero_2} é igual a {valor_total}")
