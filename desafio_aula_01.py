3ef90bedcc78916175b43c3033a7161195581ac1
Anderson Brito de Carvalho <andersoncarv@hotmail.com>
2026-01-28 11:09:53 -0300

    Tratamento de entrada de Nome

---
+++ desafio_aula_01.py
@@ -0,0 +1,10 @@
+
+# Verifica se o nome informado é Válido (Apenas Letras )
+nome_informado = input("Digite o seu nome: ")
+
+if nome_informado.isalpha():
+    print("O nome informado é válido.")
+else:
+    print("O nome informado é inválido. Por favor, use apenas letras.")
+
+
