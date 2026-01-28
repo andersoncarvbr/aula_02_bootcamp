a4b50eb7e735c4dfbcf45321204c4c0594686055
Anderson Brito de Carvalho <andersoncarv@hotmail.com>
2026-01-27 21:08:33 -0300

    Exercicio 06

--- exercicios_01.py
+++ exercicios_01.py
@@ -1,4 +1,4 @@
-## 1 - Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+### 1 - Escreva um programa que soma dois números inteiros inseridos pelo usuário.
 
 # numero_1 = int(input("Digite o primeiro valor: "))
 # numero_2 = int(input("Digite o segundo valor: "))
@@ -8,7 +8,7 @@
 # print(f" A Soma entre o valor {numero_1} e o valor {numero_2} é igual a {valor_total}")
 
 
-# ## 2 - Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+### 2 - Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 
 # numero_informado = int(input("Digite um número:"))
 
@@ -16,7 +16,7 @@
 
 # print(f" O Resto da divisão do número {numero_informado}, por 5 é igual a {resto_divisao}")
 
-# ## 3 - Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+### 3 - Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 
 # numero_1 = int(input("Digite o primeiro valor:"))
 # numero_2 = int(input("Digite o segundo valor:"))
@@ -25,7 +25,7 @@
 
 # print(f"A Multiplicação entre o valor {numero_1} e o valor {numero_2} é igual a {valor_multiplicacao}")
 
-# ## 4 - Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+### 4 - Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 
 # numero_1 = int(input("Digite o primeiro valor:"))
 # numero_2 = int(input("Digite o segundo valor:"))
@@ -33,9 +33,22 @@
 # print(f"A Divisão inteira entre o valor {numero_1} e o valor {numero_2} é igual a {valor_divisao_inteira}")
 
 
-## 5 - Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+### 5 - Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
-numero_informado = int(input("Digite um número:"))
-valor_quadrado = numero_informado ** 2
-print(f"O Quadrado do número {numero_informado} é igual a {valor_quadrado}")
+# numero_informado = int(input("Digite um número:"))
+# valor_quadrado = numero_informado ** 2
+# print(f"O Quadrado do número {numero_informado} é igual a {valor_quadrado}")
+
+###  6 - Escreva um programa que receba dois números flutuantes e realize sua adição.
+numero_1 = float(input("Digite o primeiro valor:"))
+numero_2 = float(input("Digite o segundo valor:"))  
+valor_adicao = numero_1 + numero_2
+print(f"A Adição entre o valor {numero_1} e o valor {numero_2} é igual a {valor_adicao}")
+
+
+
+###  7 -Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+###  8 -Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+###  9 -Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+### 10 -Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
 
