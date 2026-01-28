47f113f324284ccacdc83d8fd85a9951ae61c4bb
Anderson Brito de Carvalho <andersoncarv@hotmail.com>
2026-01-28 11:17:15 -0300

    Ttratando nomes inseridos com menos de 3 caracteres

--- desafio_aula_01.py
+++ desafio_aula_01.py
@@ -2,10 +2,12 @@
 # Verifica se o nome informado é Válido (Apenas Letras )
 nome_informado = input("Digite o seu nome: ")
 
-if nome_informado.isalpha():
+if nome_informado.isalpha() and len(nome_informado) >= 3:
     print("O nome informado é válido.")
 elif nome_informado == "":
-    print("Nenhum nome foi informado.")         
+    print("Nenhum nome foi informado.")
+elif len(nome_informado) < 3:
+    print("O nome informado é muito curto. Deve ter pelo menos 3 letras.")             
 else:
     print("O nome informado é inválido. Por favor, use apenas letras.")
 
