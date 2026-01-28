c213c9f88669007c94ac6830f5024d46eeb0f03c
Anderson Brito de Carvalho <andersoncarv@hotmail.com>
2026-01-28 11:12:18 -0300

    Tratamento campo no nome vazio

--- desafio_aula_01.py
+++ desafio_aula_01.py
@@ -4,6 +4,8 @@ nome_informado = input("Digite o seu nome: ")
 
 if nome_informado.isalpha():
     print("O nome informado é válido.")
+elif nome_informado == "":
+    print("Nenhum nome foi informado.")         
 else:
     print("O nome informado é inválido. Por favor, use apenas letras.")
 
